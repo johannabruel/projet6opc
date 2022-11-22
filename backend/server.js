f96dfa0e73685db1d600c1efd05c67906161c4c5
Johanna Bruel <johannabruel@gmail.com>
2022-11-22 12:13:27 +0100

    change

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-// Importe le package HTTP natif de Node
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
