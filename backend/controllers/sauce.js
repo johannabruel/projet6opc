6825133c422632380f62f0dba0488f0978126463
Johanna Bruel <johannabruel@gmail.com>
2022-12-16 17:22:03 +0100

    changes

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,11 +1,26 @@
 const Sauce = require('../models/sauce'); //Import du modèle Sauce
 const fs = require('fs');
 
+// Regex pour controller la saisie par l'utilisateur
+const regex = /^[a-zA-Z0-9 _.,!&()]+$/;
+
 // Fonction pour la création de la sauce
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id; // Supprime champ id de l'objet (généré automatiquement pas BD)
     delete sauceObject._userId; // Supprimer champ userId (pour utiliser userID du Token d'authentification)
+    
+    // Validation des données saisies dans le formulaire
+    if(
+        !regex.test(sauceObject.name) ||
+        !regex.test(sauceObject.manufacturer) ||
+        !regex.test(sauceObject.description) ||
+        !regex.test(sauceObject.mainPepper) ||
+        !regex.test(sauceObject.heat)
+    ){
+        return res.status(500).json({error: "Des caracrères saisies ne sont pas autorisés"})
+    }
+    
     const sauce = new Sauce ({ //Création objet 
         ...sauceObject,
         userId: req.auth.userId, // Extrait le userID de la requête grace multer
@@ -59,8 +74,16 @@ exports.modifySauce = (req, res, next) => { //Selon utilisateur a envoyé un fic
 
     } : {...req.body};// Sinon récupère simplement l'objet dans le corps de la requête
 
-    console.log(req.body);
-    console.log(req.params);
+    // Validation des données saisies avec la regex
+    if(
+        !regex.test(sauceObject.name) ||
+        !regex.test(sauceObject.manufacturer) ||
+        !regex.test(sauceObject.description) ||
+        !regex.test(sauceObject.mainPepper) ||
+        !regex.test(sauceObject.heat)
+    ){
+        return res.status(500).json({error: "Des caracrères saisies ne sont pas autorisés"})
+    }
     
     delete sauceObject._userId; // Supprime UserID venant de la requête (mesure sécurité)
     Sauce.findOne({_id: req.params.id}) // Cherche objet dans BD
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,27 +2,50 @@ const bcrypt = require('bcrypt'); // Package pour le chiffrement
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-// Variable d'environnement
-const dotenv = require('dotenv').config();
+// Pour la sécurité 
+const dotenv = require('dotenv').config(); // Variable d'environnement
+const emailValidator = require('email-validator'); // Vérifier que la saisie du nom utilisateur est un email
+const passwordValidator = require('password-validator'); // Vérifier que le mot de passe est assez fort
+const cryptoJs = require('crypto-js'); // Chiffrement de l'email dans la base de donnée
+
+// Création d'un schéma pour la validation du mot de passe 
+var schema = new passwordValidator();
+schema
+.is().min(5)
+.is().max(40)
+.has().digits(1)
+.has().uppercase()  
+.has().not().spaces() ;
 
 // Fonction pour enregistrer les nouveaux utilisateurs
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email:req.body.email,
-                password: hash
-            });
-        user.save()
-            .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
-            .catch(error => res.status(400).json({error}));
-    })
-    .catch(error => res.status(500).json({ error }));
+    if(!emailValidator.validate(req.body.email)){
+        return res.status(401).json({ error: "L'adresse email saisie n'est pas valide (saisir par ex: anonyme@gmail.com"})
+
+    } else if(!schema.validate(req.body.password)){
+        return res.status(401).json({ error: "Le mot de passe saisi n'est pas valide (saisir un mot de passe avec au minimum 5 caractères dont un chiffre et une majuscule"})
+        
+    } else {
+        bcrypt.hash(req.body.password, 10)
+            .then(hash => {
+                const user = new User({
+                    email:cryptoJs.HmacSHA256(req.body.email, process.env.CRYPTOJS_EMAIL_KEY).toString(), // Cryptage de l'email
+                    password: hash
+                });
+            user.save()
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
+                .catch(error => res.status(400).json({error}));
+        })
+        .catch(error => res.status(500).json({ error }));
+    }
 };
 
 // Fonction pour connecter des utilisateurs existants
 exports.login = (req, res, next) => {
-    User.findOne({email: req.body.email})
+    // Constante pour rechercher l'email crypté 
+    const cryptoSearchEmail = cryptoJs.HmacSHA256(req.body.email, process.env.CRYPTOJS_EMAIL_KEY).toString();
+    
+    User.findOne({email: cryptoSearchEmail})
     .then(user => {
         if(!user){
             return res.status(401).json({ error: 'Paire identifiant/mot de passe incorrecte'});
